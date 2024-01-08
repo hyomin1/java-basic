9ef70e8a2e35689a8d009f6a677f375afce0cbab
leehm123456 <banana8967@naver.com>
2024-01-09 02:42:34 +0900

    패키지

---
+++ src/pack/PacakgeMain1.java
@@ -0,0 +1,8 @@
+package pack;
+
+public class PacakgeMain1 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        pack.a.User user = new pack.a.User();
+    }
+}
---
+++ src/pack/PacakgeMain2.java
@@ -0,0 +1,13 @@
+package pack;
+
+//import pack.a.User;
+
+import pack.a.*;
+
+public class PacakgeMain2 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        User user = new User(); //import 사용으로 패키지명 생략 가능
+
+    }
+}
